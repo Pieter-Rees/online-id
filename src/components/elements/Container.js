40206ab8742c44b56d8d9a40bf409f424555a320
pieter Rees <pieter@pieterrees.nl>
2022-01-04 23:16:17 +0100

    mobile fix

--- src/components/elements/Container.js
+++ src/components/elements/Container.js
@@ -1,6 +1,6 @@
 const Container = (props) => (
     <section
-        className={`container mx-auto 
+        className={`container mx-auto px-8 lg:px-0 
         ${props.fullHeight ? 'h-screen' : ''}`}
     >
         {props.content}
