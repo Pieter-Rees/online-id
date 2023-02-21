61045de88e97ea4a6e6e32333732afe27e7b455b
¨Pieter <pieter@pieterrees.nl>
2023-02-21 20:45:33 +0100

    ¨color¨

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,36 @@
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  media: false, // or 'media' or 'className'
-  theme: {
-    cursor: { crosshair: 'crosshair' },
-    fontSize: {
-      base: '1.2rem',
-      lg: '1.6rem',
-      xl: '3rem',
-      xxl: '4rem',
-      xxxl: '5rem',
-      xxxxl: '6rem',
-      xxxxxl: '7rem',
-      xxxxxxl: '8rem'
+    darkMode: 'class',
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    media: false, // or 'media' or 'className'
+    theme: {
+        cursor: { crosshair: 'crosshair' },
+        fontSize: {
+            base: '1.2rem',
+            lg: '1.6rem',
+            xl: '3rem',
+            xxl: '4rem',
+            xxxl: '5rem',
+            xxxxl: '6rem',
+            xxxxxl: '7rem',
+            xxxxxxl: '8rem',
+        },
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+            xxl: '2560px',
+        },
+        colors: {
+            white: '#f8f8ff',
+            black: '#000',
+            red: '#e53e3e',
+            blue: '#89cff0',
+            green: '#9bdb69',
+        },
     },
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-      xxl: '2560px'
+    variants: {
+        extend: {},
     },
-    colors: {
-      white: '#f8f8ff',
-      black: '#000',
-      red: '#e53e3e'
-    }
-  },
-  variants: {
-    extend: {}
-  },
-  plugins: []
+    plugins: [],
 }
