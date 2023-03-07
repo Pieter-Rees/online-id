41dbf663000a0bb340f0bcd7dc57d1a4b341e1ea
¨Pieter <pieter@pieterrees.nl>
2023-03-07 23:14:43 +0100

    works, not using typechec

--- src/components/segments/ResumeContent.tsx
+++ src/components/segments/ResumeContent.tsx
@@ -1,19 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ReactNode } from 'react';
+
+interface Powers {
+    name: string;
+}
+interface DataInterface {
+    title?: string;
+    subTitle?: string;
+    powers?: Powers[];
+}
 
 function ResumeContent() {
-
-    const [data, setData] = useState();
+    const [data, setData] = useState<DataInterface[]>([])
 
     useEffect(() => {
-        fetch('https://pietserver.nl:5050/resume/')
+        fetch('http://localhost:5050/test/')
             .then(response => response.json())
             .then(data => setData(data));
-        console.log(data)
     }, [])
 
     return (
-        <div>
-            {data}
+        <div className='h-full' id='landing'>
+            {data &&
+                <ul className='flex justify-center flex-wrap items-stretch'>
+                    {data.map((data, index) =>
+                        <li className='w-96 m-8' key={index}>
+                            <h3 className='text-xl'>
+                                {data.title}
+                            </h3>
+                            {data.subTitle &&
+                                <h4 className='text-lg'>
+                                    {data.subTitle}
+                                </h4>}
+                            <ul >
+                                {data.powers && data.powers.map((powers, index) => <li className='text-base' key={index}>{powers.name}</li>)}
+                            </ul>
+
+                        </li>)}
+                </ul>
+            }
         </div>
     )
 }
